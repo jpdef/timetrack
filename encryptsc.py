11f210b712a09a2f108bcaf231ce393b61fa9a4d
jpdef <jacobdefilippis@gmail.com>
2016-05-26 15:58:39 -0700

    initial

---
+++ encryptsc.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.7
+import pyunix
+import subprocess
+import string
+import os
+import time
+import datetime
+
+today = datetime.date.today().strftime("%d-%m-%y")
+
+def encrypt(uid,fname):
+  cmd = "gpg";
+  uargs=["--armor","-r",uid,"--encrypt",fname]
+  return pyunix.docmd(cmd,uargs)
+
+def decrypt(fname):
+  cmd = "gpg"
+  uargs=["--decrypt",fname]
+  return pyunix.docmd(cmd,uargs)
+
+def gitdiff():
+  cmd = "git"
+  uargs=["--no-pager","diff","master","origin/master"]
+  return pyunix.docmd(cmd,uargs) 
+
+def gitpush():
+  cmd = "git"
+  uargs=["push","origin","master"]
+  return pyunix.docmd(cmd,uargs) 
+
+def gitpull():
+  cmd = "git"
+  uargs=["pull","origin","master"]
+  return pyunix.docmd(cmd,uargs)
+
+def gitcommit(comment):
+  cmd = "git"
+  uargs=["commit","-a","-m",comment]
+  return pyunix.docmd(cmd,uargs)
+
