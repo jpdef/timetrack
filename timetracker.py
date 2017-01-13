1a471083ee48cc479ff108ab5ceb047f185c9155
Jacob Paul De filippis <jpdef@icd-pc89947.slac.stanford.edu>
2017-01-13 15:42:01 -0800

    source files

---
+++ timetracker.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python2.7
+import pyunix
+import sys
+import os
+import subprocess
+import csv
+import time
+import datetime
+import encryptsc
+import argparse
+
+class tracker:    
+    def __init__(self):
+        self.data_dir = os.environ.get("TIMETRACKPATH")
+        self.month_file = self.data_dir + "/january.csv"
+        self.month_file_scramble = self.data_dir + "/january.csv.asc"
+        self.todayCommit = datetime.date.today().strftime("%d-%m-%y")
+
+        self.db = []
+        with open(self.month_file,"rb") as trackfile:
+            reader = csv.DictReader(trackfile)
+            for row in reader:
+                self.db.append(row)
+    
+    def print_day(self,dates):
+        datelist = self.getDates(dates)
+        total = 0 
+        for item in datelist:
+            itemtime = self.calcTimeRow(item)
+            print "{} : {} minutes : {} : {}".format(item['date'],itemtime,item['subject'],item['description'] )
+            total += itemtime
+        print "Total := {} minutes".format(total)
+     
+    def print_week(self,dates):
+        pass
+   
+ 
+    def how_many_minutes(self,start,end):
+        hstart , mstart = start.tm_hour, start.tm_min
+        hend   , mend = end.tm_hour, end.tm_min
+        
+        return (hend - hstart)*60 + (mend -mstart)
+
+    def write_start(self,subject):
+        with open(month_file,"a") as trackfile:
+            trackwriter = csv.writer(trackfile, delimiter=',')
+            trackwriter.writerow([subject,datetime.datetime.now().strftime("%H:%M")])
+    
+    def write_date(self):
+        with open(self.month_file,"a") as trackfile:
+            trackwriter = csv.writer(trackfile, delimiter=',')
+            trackwriter.writerow([datetime.datetime.now().strftime("%m-%d")])
+
+    def calcTimeRow(self,d):
+        start = time.strptime(d['start'], "%H:%M")
+        end   = time.strptime(d['end'], "%H:%M")
+        return  self.how_many_minutes(start,end)
+
+    def getDates(self,dates):
+        datelist = []
+        for date in dates:
+            datelist += self.getDate(date)
+        datelist = filter(lambda x : x['subject'] in self.tickets, datelist ) 
+        return datelist 
+
+    def getDate(self,strdate):
+        time = []
+        for d in self.db:
+            if (d['date'] == strdate):
+                time.append(d) 
+        return time
+
+    def update_remote(self,forWhom):
+        whom = [w for w in forWhom]
+        encryptsc.encrypt(whom,self.month_file)
+        encryptsc.gitcommit(self.todayCommit,self.data_dir)
+        encryptsc.gitpush(self.data_dir)
+    
+    def update_local(self):
+      encryptsc.gitpull(self.data_dir)
+      encryptsc.decrypt(self.month_file_scramble,self.month_file)
+    
+    def addFn(self,content):
+      with open(self.month_file,"a") as trackfile:
+          trackwriter = csv.writer(trackfile, delimiter=',')
+          now  = datetime.datetime.now().strftime("%D")
+          content.insert(0,now)
+          trackwriter.writerow(content)
